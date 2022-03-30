3130104d2097f1c1725b0219ee0e3c6eaac1b923
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-03-31 00:26:28 +0300

    Developing a reports system for my church, Abundant Life Churc-- my first step.

---
+++ index.php
@@ -0,0 +1,21 @@
+<!--To change the script to a HTML script, type HTML!, and hit the space button-->
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+<!--    Bootstrap-->
+    <script src="assets/bootstrap/js/jquery-3.4.0.js"></script>
+    <script src="assets/bootstrap/js/popper.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.js"></script>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css">
+<!--    Custom styles-->
+    <link rel="stylesheet" href="assets/custom_styles.css">
+</head>
+<body>
+
+</body>
+</html>
